e10a22364db625001f95b9c6513d6cfbb69b2c86
Thiago <prattithiago5@gmail.com>
2023-09-26 16:28:33 -0300

    HEADER Finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import IgniteLogo from "../assets/Ignite.svg" 
+
 export function Header(){
     return(
         <header className={styles.headerM}>
-            <h1>Ignite Feed</h1>
+            <img src={IgniteLogo} alt="Logo" />
         </ header>
     )
 }
