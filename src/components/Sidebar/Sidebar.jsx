0c9a7807f859d2d6888fb0d33c8fe4d208a98a3c
Thiago <prattithiago5@gmail.com>
2023-09-27 14:53:49 -0300

    SIDEBAR FINISHED

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,21 @@
-import styles from "./Sidebar.module.css"
+import styles from "./Sidebar.module.css";
+import { TbPencilMinus } from "react-icons/tb";
 
 export function Sidebar(){
     return(
         <aside className={styles.sidebar}>
 
-            <img src="https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
              />
 
              <div className={styles.profile}>
-                <strong></strong>
+                <img className={styles.avatar} src="https://github.com/Prattiz.png" alt="" />
+                <strong>Thiago Pratti de Mendonça</strong>
                 <span>Web Developer</span>
              </div>
 
              <footer>
-                <a href="#">Editar seu Perfil</a>
+                <a href="#"><TbPencilMinus/>  Editar seu Perfil</a>
              </footer>
         </aside>
     )
